cd68164cfe9f765c7d9075dcb99bc3b7592bd7f4
Chris Lovett <clovett@microsoft.com>
2025-08-08 16:49:00 -0700

    Factor power state management into separate StateMachine class for easier testing and cleanup of lighting_designer code.

--- Server/lighting_designer.py
+++ Server/lighting_designer.py
@@ -13,6 +13,7 @@ from suntime import Sun
 from animation import AnimationLoop
 from logger import Logger
 from priority_queue import PriorityQueue
+from state_machine import StateMachine, States
 from utilities import TimedLatch
 
 logger = Logger()
@@ -33,7 +34,6 @@ class LightingDesigner:
         self.msgbus = msgbus
         self.internet_address = internet_address
         msgbus.add_listener(self.onmessage)
-        self.entered_custom_state: float = 0.0
         self.msgqueue = PriorityQueue()
         self.server = server
         self.sensei = sensei
@@ -42,20 +42,28 @@ class LightingDesigner:
         self.thread = None
         self.zone_maps = {}
         self.enable_movement = True
+        self.power_state = "initiailizing"
+        self.state_machine = StateMachine(
+            self.config.on_days,
+            self.config.on_time,
+            self.config.off_time,
+            self.config.latitude,
+            self.config.longitude,
+            self.config.timezone,
+            self.config.turn_off_timeout,
+            self.config.custom_animation_timeout,
+            self.config.reboot_timeout,
+        )
         self.movement_latch = TimedLatch(10)
         self.core_is_on = False
         self.last_change: float = 0.0
         self.last_cool_animation: float = 0.0
         self.last_color = None
         self.last_camera_emotion = 0
-        self.turn_off_time = None
-        self.auto_reboot_time = None
         self.animations = None
         self.power_state = "initializing"
-        self.power_on_override = None
-        self.power_off_override = None
-        self.power_off_latch = False  # is True when power is on, when this goes to false we can reset power_on_override
-        self.reboot = False
+        self.bridge_initialized = False
+        self.cooling = False
         self.color_override = None
         self.is_raining = None
         self.stop_rain_time = None
@@ -156,65 +164,39 @@ class LightingDesigner:
         self.msgqueue.enqueue(0, msg)
 
     def _set_power_state(self, option):
-        self.power_state = option
         if option == "on":
             log.info("### power on override")
-            self._master_power_on()
-            self.power_on_override = True
-            self.power_off_override = False
+            self.state_machine.set_state(States.CUSTOM)
             self.last_change = 0
             self.last_camera_emotion = 0
             self.last_cool_animation = 0
-            self.turn_off_time = None
             self.msgbus.send("/state/on")  # broadcast to all clients
         elif option == "off":
             log.info("### power off override")
-            self.power_on_override = False
-            self.power_off_override = True
+            self.power_state = self.state_machine.turn_off()
             self.color_override = False
             self.animations = None
-            self.auto_reboot_time = None
             self._fade_to_black()
-            self.turn_off_time = None
         elif option == "run":
             log.info("### back to normal operation")
-            self.power_on_override = False
-            self.power_off_override = False
+            self.state_machine.set_state(States.ON)
             self.color_override = False
-            self.turn_off_time = None
-            self.auto_reboot_time = time.time() + (self.config.auto_reboot * 3600)
             self.animations = None
             self.msgbus.send("/state/run")  # broadcast to all clients
         elif option == "custom":
             # user is doing a custom control, so keep or turn the lights on
             # to show this custom animation.  The this custom state will
-            # remain until some timeout past self.entered_custom_state.
-            self.power_on_override = True
-            self.power_off_override = False
-            self.auto_reboot_time = None
-            self.entered_custom_state = time.time()
+            # remain until the configured custom_timeout.
+            self.state_machine.set_state(States.CUSTOM)
         elif option == "reboot":
             log.info("### rebooting...")
-            self.power_on_override = False
-            self.power_off_override = True
-            self.auto_reboot_time = None
+            self.state_machine.set_state(States.REBOOT)
             self.color_override = False
             self.animations = None
             self.reboot = True
             self._fade_to_black()
-            self.turn_off_time = None
-            self.entered_custom_state = 0
+            self.state_machine.set_state(States.REBOOT)
             self.msgbus.send("/state/reboot")
-        elif option == "rebooted":
-            log.info("### reboot completed")
-            self.reboot = False
-            self.power_on_override = False
-            self.power_off_override = False
-            self.color_override = False
-            self.turn_off_time = None
-            self.animations = None
-            self.power_state == "on"
-            self.msgbus.send("/state/rebooted")
         else:
             log.error("error: invalid power state: /power/{}".format(option))
 
@@ -226,7 +208,9 @@ class LightingDesigner:
                 option = "on"
         if option == "on":
             self._set_power_state("custom")
-            self.server.queue_command(0, {"command": "StartRain", "size": 12, "amount": 50.0})
+            self.server.queue_command(
+                0, {"command": "StartRain", "size": 12, "amount": 50.0}
+            )
             self.is_raining = True
             self.stop_rain_time = None
         elif option == "off":
@@ -257,7 +241,9 @@ class LightingDesigner:
         # every remote command turns ada on, except for the "run" command which
         # puts ada back on program according to config.
         if parts[0] == "ping":
-            self.msgbus.send(f"{pingPrefix}/state/{self.power_state} at {self.internet_address}")
+            self.msgbus.send(
+                f"{pingPrefix}/state/{self.power_state} at {self.internet_address}"
+            )
         elif parts[0] == "bridge":
             bridge = self.server.get_bridge()
             if not bridge:
@@ -342,7 +328,9 @@ class LightingDesigner:
                         colors += [color]
                     i += 1
                 if len(colors) > 0:
-                    log.info("### gradient {} {} colors: {}".format(target, strip, colors))
+                    log.info(
+                        "### gradient {} {} colors: {}".format(target, strip, colors)
+                    )
                     self._add_gradient(target, strip, colorsPerStrip, colors, seconds)
                     self.color_override = True
                     self.animations = None
@@ -395,6 +383,16 @@ class LightingDesigner:
             if bridge:
                 # a kind of heart beat to keep the bridge socket alive.
                 bridge.update_switch_status()
+                if not self.bridge_initialized and bridge.lights_on is not None:
+                    self.bridge_initialized = True
+                    self.power_state = States.ON if bridge.lights_on else States.OFF
+                    self.state_machine.set_state(self.power_state)
+
+            if not self.bridge_initialized:
+                log.info("### waiting for bridge to find the Kasa Power Switches...")
+                time.sleep(60)
+                continue
+
             new_clients = self.server.get_stale_clients()
             has_new_clients = len(new_clients) > 0
 
@@ -403,38 +401,33 @@ class LightingDesigner:
             except Exception as e:
                 log.error("error with process_next_message: " + str(e))
 
-            if (
-                self.power_state == "custom"
-                and time.time() > self.entered_custom_state + self.config.custom_animation_timeout
-            ):
-                # timeout custom animations and go back to normal "run" mode so Ada can go to sleep
-                # if necessary.
-                self._set_power_state("run")
+            self.power_state = self.state_machine.advance(time.time())
 
-            if (
-                self.power_state == "rebooted"
-                and self.entered_custom_state != 0
-                and time.time() > self.entered_custom_state + self.config.reboot_timeout
-            ):
-                self._set_power_state("rebooted")
+            if self.power_state == States.COOL_DOWN:
+                if not self.cooling:
+                    self.cooling = True
+                    log.info(
+                        "### cooling down for {} seconds".format(
+                            self.config.turn_off_timeout
+                        )
+                    )
+                    self.server.clear_queue()
+                    self._fade_to_black()
+                    self.color_override = False
+                    self.animations = None
+                    self.sensei.stop()  # no need to keep pinging cosmos while we are sleeping.
+                continue
 
-            if self.auto_reboot_time and time.time() > self.auto_reboot_time:
-                self._set_power_state("reboot")
+            if self.power_state == States.CUSTOM and self.reboot:
+                log.info("### reboot completed")
+                self.reboot = False
+                self.color_override = False
+                self.animations = None
+                self.msgbus.send("/state/rebooted")
 
-            # highest priority is the master power schedule
-            master_power_state = self._get_master_power_state()
+            master_power_state = self.power_state in [States.ON, States.CUSTOM]
             if master_power_state:
-                self.power_off_latch = True
-            elif self.power_off_latch:
-                # we were supposed to be on, now we are supposed to be off,
-                # so if someone set self.power_off_override to True then this is a
-                # good time to reset that to False so the lights turn on again tomorrow.
-                log.info("### resetting power_off_override")
-                self.power_off_override = False
-                self.power_off_latch = False  # this is a one time latch.
-
-            if (master_power_state or self.power_on_override) and not self.power_off_override:
-                if bridge and (bridge.lights_on is None or not bridge.lights_on):
+                if bridge and (not bridge.lights_on or has_new_clients):
                     # looks like we need to turn the lights on
                     self._master_power_on()
                     # if we just did a power cycle then reset any previous color overrides.
@@ -442,43 +435,15 @@ class LightingDesigner:
                     self.animations = None
                     log.info("### back to normal operation")
                     self.power_state = "on"
-                    self.auto_reboot_time = time.time() + (self.config.auto_reboot * 3600)
                     self.sensei.start()
                 self.server.camera_on()
-            elif (not master_power_state or self.power_off_override) and not self.power_on_override:
-                if bridge and (bridge.lights_on is None or bridge.lights_on or has_new_clients):
+            else:
+                if bridge and (bridge.lights_on or has_new_clients):
                     # looks like we need to turn the lights off
-                    if self.turn_off_time is None or has_new_clients:
-                        log.info("### cooling down for {} seconds".format(self.config.turn_off_timeout))
-                        self.turn_off_time = time.time() + self.config.turn_off_timeout
-                        self.server.clear_queue()
-                        self._fade_to_black()
-                        self.color_override = False
-                        self.animations = None
-                        self.power_state = "cooling"
-                        self.auto_reboot_time = None
-                        self.sensei.stop()  # no need to keep pinging cosmos while we are sleeping.
-                        continue
-                    elif time.time() > self.turn_off_time:
-                        self.server.clear_queue()
-                        self._master_power_off()
-                        self.color_override = False
-                        self.animations = None
-                        self.turn_off_time = None
-                        self.auto_reboot_time = None
-                        if self.reboot:
-                            # let it be off for config.reboot_timeout seconds, then turn it back on.
-                            self.entered_custom_state = time.time()
-                            self.power_state = "rebooted"
-                        else:
-                            self.power_state = "off"
-                            self.sensei.stop()  # no need to keep pinging cosmos while we are sleeping.
-                            on_hour, on_minute = self.on_time
-                            log.info("### lights are off, waiting for wake up time: {}:{}".format(on_hour, on_minute))
-                    else:
-                        # let the lights cool down for 3 minutes before turning them off.
-                        time.sleep(0.1)
-                        continue
+                    self.server.clear_queue()
+                    self._master_power_off()
+                    self.color_override = False
+                    self.animations = None
 
                 self.server.camera_off()
                 time.sleep(0.1)
@@ -556,7 +521,10 @@ class LightingDesigner:
                             if self.movement_latch.switch(movement == "movement"):
                                 log.info("### movement detected!")
                                 # enqueue a new start rain command (but only if it is not already raining!)
-                                if self.stop_rain_time is None or self.stop_rain_time < time.time():
+                                if (
+                                    self.stop_rain_time is None
+                                    or self.stop_rain_time < time.time()
+                                ):
                                     self.server.queue_command(
                                         0,
                                         {
@@ -565,12 +533,18 @@ class LightingDesigner:
                                             "amount": 50.0,
                                         },
                                     )
-                                self.stop_rain_time = time.time() + self.config.movement_rain_timeout
+                                self.stop_rain_time = (
+                                    time.time() + self.config.movement_rain_timeout
+                                )
                                 self.last_change = time.time()
 
             if not self.color_override and self._get_cool_animation_time():
                 self.last_change = time.time()
-                if self.last_cool_animation + self.config.cool_animation_timeout > time.time() and not has_new_clients:
+                if (
+                    self.last_cool_animation + self.config.cool_animation_timeout
+                    > time.time()
+                    and not has_new_clients
+                ):
                     continue
                 num = len(self.config.cool_animations) - 1
                 animation = None
@@ -596,7 +570,11 @@ class LightingDesigner:
             if has_new_clients:
                 if self.last_color is None:
                     self.last_color = [0, 0, 0]
-                log.info("### Clients {} are out of date, replaying the last color".format(new_clients))
+                log.info(
+                    "### Clients {} are out of date, replaying the last color".format(
+                        new_clients
+                    )
+                )
                 for target in new_clients:
                     self._set_color(target, self.last_color)
 
@@ -605,7 +583,13 @@ class LightingDesigner:
     def _handle_camera_emotions(self, emotions):
         # IpCameraGui could send something like this, depending on how many faces are looking at the camera:
         # ['Happiness,0.9999596', 'Happiness,0.997026', 'Happiness,0.7164325', 'Happiness,0.703617']
-        no_scores = [x.strip() for x in emotions.replace("[", "").replace("]", "").replace("'", "").split(",")][::2]
+        no_scores = [
+            x.strip()
+            for x in emotions.replace("[", "")
+            .replace("]", "")
+            .replace("'", "")
+            .split(",")
+        ][::2]
         # just show the most popular emotion.
         if len(no_scores) > 1:
             value, count = Counter(no_scores)
@@ -615,22 +599,15 @@ class LightingDesigner:
             value = no_scores[0]
         if value:
             log.info("### Highlighting camera emotion: {}".format(value))
-            self._blush(None, value, seconds=2, hold=self.config.hold_camera_blush, priority=5)
-
-    def _get_master_power_state(self):
-        if not self.on_today:
-            return False
-        on_hour, on_minute = self.on_time
-        off_hour, off_minute = self.off_time
-        now = datetime.now()
-        if (now.hour > on_hour or (now.hour == on_hour and now.minute >= on_minute)) and (
-            now.hour < off_hour or (now.hour == off_hour and now.minute < off_minute)
-        ):
-            return True
-        return False
+            self._blush(
+                None, value, seconds=2, hold=self.config.hold_camera_blush, priority=5
+            )
 
     def _get_cool_animation_time(self):
-        if self.server.idle() and self.last_change + self.config.cool_animation_timeout < time.time():
+        if (
+            self.server.idle()
+            and self.last_change + self.config.cool_animation_timeout < time.time()
+        ):
             return True
 
         on_hour, on_minute = self.config.cool_animation_time
@@ -720,7 +697,9 @@ class LightingDesigner:
         self.server.queue_command(1, commands)
         self.last_change = time.time()
 
-    def _add_gradient(self, target, strip, colorsPerStrip, colors, seconds, priority=10):
+    def _add_gradient(
+        self, target, strip, colorsPerStrip, colors, seconds, priority=10
+    ):
         if target == "DMX":
             return  # not supported on dmx at the moment...
         command = {"command": "Gradient", "colors": colors, "seconds": seconds}
@@ -736,7 +715,9 @@ class LightingDesigner:
     def _fade_to_zones(self, target, emotions, priority):
         command = None
         if target == "DMX":
-            rgb_colors = self._get_rgb_colors(emotions, self.config.colors_for_dmx_emotions)
+            rgb_colors = self._get_rgb_colors(
+                emotions, self.config.colors_for_dmx_emotions
+            )
             # SET "light_1" and 2 to be dark blue
             rgb_colors[0] = [0, 0, 80]
             rgb_colors[1] = [0, 0, 80]
---
+++ Server/state_machine.py
@@ -0,0 +1,211 @@
+import time
+from datetime import datetime, timedelta, timezone
+from typing import List, Tuple
+
+from suntime import Sun  # type: ignore
+
+
+class States:
+    INITIAL = "initial"
+    OFF = "off"
+    COOL_DOWN = "cool_down"
+    CUSTOM = "custom"
+    REBOOT = "reboot"
+    ON = "on"
+    ALL = [OFF, COOL_DOWN, CUSTOM, REBOOT, ON]
+
+
+class StateMachine:
+    def __init__(
+        self,
+        on_days: List[str],
+        off_time: Tuple[int] | str,  # a hour/minute or "sunrise"/"sunset"
+        on_time: Tuple[int] | str,  # a hour/minute or "sunrise"/"sunset"
+        latitude: float,  # for resolving sunrise/sunset
+        longitude: float,
+        utc_offset: int,  # timezone offset in hours
+        turn_off_timeout: int,  # in seconds
+        custom_timeout: int,  # in seconds
+        reboot_timeout: int,  # in seconds
+    ):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timezone = timezone(timedelta(hours=utc_offset))
+        self.on_days = on_days
+        self.off_time = off_time
+        self.on_time = on_time
+        self.turn_off_timeout = turn_off_timeout
+        self.custom_timeout = custom_timeout
+        self.reboot_timeout = reboot_timeout
+        self.state = States.INITIAL
+        self.turn_off_start = 0.0  # seconds
+        self.custom_start = 0.0  # seconds since epoch
+        self.reboot_start = 0.0  # seconds since epoch
+        self.rebooting = (
+            False  # performs auto-cooldown, then off for reboot timeout then on again.
+        )
+        self.power_off_day_of_week = ""  # used to track if we are off today
+
+    def advance(self, now: float) -> str:
+        desired_state = self._get_master_power_state(now)
+
+        if self.state == States.INITIAL:
+            raise ValueError(
+                "StateMachine is in INITIAL state, cannot advance. "
+                + "Please call set_state() once to communicate actual state of the lights."
+            )
+
+        if self.state == States.COOL_DOWN:
+            if now - self.turn_off_start > self.turn_off_timeout:
+                self.set_state(States.OFF, now)
+                if self.rebooting:
+                    # if we were rebooting, we need to turn back on after the reboot timeout
+                    self.set_state(States.OFF, now)
+                    self.reboot_start = now
+        elif self.state == States.OFF and self.rebooting:
+            if now - self.reboot_start > self.reboot_timeout:
+                # if we were rebooting, we need to turn back on after the reboot timeout
+                self.set_state(States.CUSTOM, now)
+                self.rebooting = False
+        elif self.state == States.CUSTOM:
+            if now - self.custom_start > self.custom_timeout:
+                self.set_state(States.ON if desired_state else States.OFF, now)
+
+        elif self.state == States.OFF:
+            if desired_state:
+                self.set_state(States.ON, now)
+        elif self.state == States.ON:
+            if not desired_state:
+                self.set_state(States.OFF, now)
+
+        return self.state
+
+    def turn_off(self, now: float = time.time()):
+        # override the on time for the rest of the day.
+        self.power_off_day_of_week = datetime.fromtimestamp(now).strftime("%A")
+        self.set_state(States.OFF, now)
+
+    def set_state(self, new_state, now: float = time.time()) -> str:
+        if new_state in States.ALL:
+            if new_state == States.OFF and self.state == States.ON:
+                # need to cool down before turning off
+                new_state = States.COOL_DOWN
+                self.turn_off_start = now
+            elif new_state == States.CUSTOM:
+                self.custom_start = now
+            elif new_state == States.REBOOT:
+                self.reboot_start = now
+                self.rebooting = True
+                new_state = States.COOL_DOWN
+                self.turn_off_start = now
+
+            self.state = new_state
+            return new_state
+        else:
+            raise ValueError("Invalid state")
+
+    def _get_master_power_state(self, now: float):
+        if not self._get_on_today(now):
+            return False
+        on_hour, on_minute = self._resolve_sunrise_sunset(self.on_time)
+        off_hour, off_minute = self._resolve_sunrise_sunset(self.off_time)
+        d = datetime.fromtimestamp(now)
+        if (d.hour > on_hour or (d.hour == on_hour and d.minute >= on_minute)) and (
+            d.hour < off_hour or (d.hour == off_hour and d.minute < off_minute)
+        ):
+            return True
+        return False
+
+    def _get_on_today(self, now: float):
+        """Check if Ada is scheduled to be on today and if so at what start and stop times.
+        Sets the variables self.on_today, self.on_time, and self.off_time."""
+        run_days = self.on_days
+        today = datetime.fromtimestamp(now)
+        day_of_week = today.strftime("%A")
+        if self.power_off_day_of_week == day_of_week:
+            # if we turned off today, we are not on today
+            return False
+        else:
+            self.power_off_day_of_week = ""  # reset for next day
+        return day_of_week in run_days
+
+    def _resolve_sunrise_sunset(self, setting):
+        sun = Sun(self.latitude, self.longitude)
+        sunset = sun.get_sunset_time(time_zone=self.timezone)
+        sunrise = sun.get_sunrise_time(time_zone=self.timezone)
+        if setting == "sunrise":
+            return [sunrise.hour, sunrise.minute]
+        if setting == "sunset":
+            return [sunset.hour, sunset.minute]
+        return setting
+
+
+def test():
+    latitude = 47.67399000
+    longitude = -122.12151000
+    timezone = -7
+    machine = StateMachine(
+        ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        "sunset",
+        "sunrise",
+        latitude,
+        longitude,
+        timezone,
+        60,
+        30,
+        10,
+    )
+
+    # pick a Friday so we roll into the weekend
+    date = datetime(2025, 8, 1, 12, 0, 0, 0)
+    now = date.timestamp()
+    machine.set_state(States.OFF)
+    custom = False
+    reboot = False
+    current = date.day
+    # simulate 5 days we should see two days off for the weekend then on again on Monday
+    for i in range(24 * 5):
+        now += 3600
+        date = datetime.fromtimestamp(now)
+        if date.day != current:
+            current = date.day
+            print(
+                f"New day: {date}======================================================"
+            )
+        state = machine.advance(now)  # Advance by 1 hour
+        print(f"{date}: {state}")
+        if state == States.COOL_DOWN:
+            # make sure cool down state latches for turn off timeout
+            while state == States.COOL_DOWN:
+                now += 10
+                state = machine.advance(now)  # Advance by 10 seconds
+                print(f"{date}: {state} (cool down)")
+
+        elif state == States.ON and not custom:
+            custom = True
+            state = States.CUSTOM
+            machine.set_state(States.CUSTOM, now)
+            print(f"{date}: {state} (custom)")
+            # make sure custom state latches for custom timeout
+            while state == States.CUSTOM:
+                now += 10
+                state = machine.advance(now)  # Advance by 10 seconds
+                print(f"{date}: {state} (custom)")
+
+        elif state == States.ON and not reboot:
+            reboot = True
+            state = States.REBOOT
+            machine.set_state(States.REBOOT, now)
+            print(f"{date}: {state} (reboot)")
+            # make sure reboot state latches for cool down timeout + reboot timeout
+            while state in [States.COOL_DOWN, States.OFF, States.REBOOT, States.CUSTOM]:
+                now += 10
+                state = machine.advance(now)  # Advance by 10 seconds
+                print(f"{date}: {state} (reboot)")
+
+            # test we can now turn off for the rest of the day.
+            machine.turn_off(now)
+
+
+if __name__ == "__main__":
+    test()
